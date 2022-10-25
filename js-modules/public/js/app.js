8d00f50967c19fd2ec1f6a8d22ffbd9857b04eb9
arpitbhardwaj <arpitbhardwaj09@gmail.com>
2022-10-25 11:57:51 +0530

    Corrected Formatting

--- js-modules/public/js/app.js
+++ js-modules/public/js/app.js
@@ -1,23 +1,21 @@
 //import getSessions, { sessionTemplate, sessionURL } from './sessionRepository.js'
-import getSessions, * as sessionRepository from './sessionRepository.js'
+import getSessions, * as sessionRepository from "./sessionRepository.js";
 //import { sessionTemplate, errorMessage } from './template.js'
 //import * as template from './template.js'
 
 function render() {
-    var list = document.querySelector('#sessions');
-    if (!list) return;
-    //list.innerHTML = template.sessionTemplate(data.listItems);
-    list.innerHTML = sessionRepository.sessionTemplate(data.listItems);
-};
-
+  var list = document.querySelector("#sessions");
+  if (!list) return;
+  //list.innerHTML = template.sessionTemplate(data.listItems);
+  list.innerHTML = sessionRepository.sessionTemplate(data.listItems);
+}
 
 var data = {
-    listItems: []
+  listItems: [],
 };
 
-getSessions()
-    .then((sessions) => {
-        console.log('promises!')
-        data.listItems = sessions;
-        render();
-    });
+getSessions().then((sessions) => {
+  console.log("promises!");
+  data.listItems = sessions;
+  render();
+});
--- js-modules/public/js/sessionRepository.js
+++ js-modules/public/js/sessionRepository.js
@@ -1,20 +1,19 @@
-const sessionURL = 'sessions.json';
+const sessionURL = "sessions.json";
 
 let sessionList = [];
 
 function getSessions() {
-    return new Promise(function(resolve, reject) {
-        var oReq = new XMLHttpRequest();
-        oReq.onload = function(e) {
-            sessionList = e.target.response;
-            resolve(sessionList);
-        };
-        oReq.open('GET', sessionURL, true);
-        oReq.responseType = 'json';
-        oReq.send();
-    })
+  return new Promise(function (resolve, reject) {
+    var oReq = new XMLHttpRequest();
+    oReq.onload = function (e) {
+      sessionList = e.target.response;
+      resolve(sessionList);
+    };
+    oReq.open("GET", sessionURL, true);
+    oReq.responseType = "json";
+    oReq.send();
+  });
 }
 
 export { getSessions as default, sessionURL };
-export { sessionTemplate }
-from './template.js'
+export { sessionTemplate } from "./template.js";
